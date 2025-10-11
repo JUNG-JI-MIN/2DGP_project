c415693e9d1e1cf45271c87f339ab1a68e83c6d5
JUNG JI MIN <wlals04977@naver.com>
2025-10-12 04:05:21 +0900

    초기 설정

---
+++ main.py
@@ -0,0 +1,19 @@
+from pico2d import *
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+running = True
+open_canvas()
+
+while running:
+    handle_events()
+
+close_canvas()

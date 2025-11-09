15b00e90398add1dfa9c1597e416f112cec88ae8
JUNG JI MIN <86883269+JUNG-JI-MIN@users.noreply.github.com>
2025-11-10 01:33:15 +0900

    점프 및 바운딩 박스 구현

--- viego.py
+++ viego.py
@@ -1,17 +1,20 @@
-from pico2d import load_image, get_time, load_font
+from pico2d import load_image, get_time, load_font, draw_rectangle
 from sdl2 import SDL_KEYDOWN, SDL_KEYUP
-from events import space_down, time_out, a_key, a_key_up,s_key_down,s_key_up, right_down, left_down, right_up, left_up
+from events import space_down, time_out, a_key, a_key_up, s_key_down, s_key_up, right_down, left_down, right_up, \
+    left_up, up_down
 from state_machine import StateMachine
 import sheet_list
 import game_framework
 
 PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm.
-RUN_SPEED_KMPH = 20.0 # Km / Hour
-RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0) # Meter / Minute
-RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0) # Meter / Second
-RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER) # 초당 픽셀 이동 거리 (Pixel Per Second)
-JUMP_HEIGHT_PSS = RUN_SPEED_PPS * 1.5 # 점프 높이 (Pixel Per Second Speed)
-DASH_SPEED_PSS = RUN_SPEED_PPS * 2 # 대쉬 속도 (Pixel Per Second Speed)
+WALK_SPEED_KMPH = 20.0 # Km / Hour
+WALK_SPEED_MPM = (WALK_SPEED_KMPH * 1000.0 / 60.0) # Meter / Minute
+WALK_SPEED_MPS = (WALK_SPEED_MPM / 60.0) # Meter / Second
+WALK_SPEED_PPS = (WALK_SPEED_MPS * PIXEL_PER_METER) # 초당 픽셀 이동 거리 (Pixel Per Second)
+JUMP_HEIGHT_PSS = WALK_SPEED_PPS * 1.5 # 점프 높이 (Pixel Per Second Speed)
+DASH_SPEED_PSS = WALK_SPEED_PPS * 2 # 대쉬 속도 (Pixel Per Second Speed)
+
+GRAVITY = 1000 / 3 # 중력 가속도 cm / s^2
 
 TIME_PER_ACTION = 1
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
@@ -38,41 +41,39 @@ class guard:
             self.viego.img.clip_composite_draw(
                 f[0], 1545 - f[1] - f[3], f[2], f[3], 0, 'h', self.viego.x, self.viego.y,f[2], f[3])
 class jump:
+
     def __init__(self, viego):
         self.viego = viego
-        self.vertical_speed = 0
-        self.horizontal_speed = 0
-        self.gravity = 800
-
+        self.velocity = JUMP_HEIGHT_PSS
     def enter(self, e):
-        self.viego.frame = 0
-        # 점프 시작 시 초기 수평 속도 설정
-        if self.viego.is_dashing:
-            self.horizontal_speed = self.viego.dir * DASH_SPEED_PSS
-        elif self.viego.dir != 0:
-            self.horizontal_speed = self.viego.dir * RUN_SPEED_PPS
-        else:
-            self.horizontal_speed = 0
-
+        self.viego.is_jumping = True
+        pass
     def exit(self, e):
-        self.viego.frame = 0
+        self.viego.is_jumping = False
+        self.viego.y = 90
         pass
-
     def do(self):
+        if self.viego.is_dashing:
+            self.viego.x += self.viego.dir * DASH_SPEED_PSS * game_framework.frame_time
+        elif self.viego.dir != 0:
+            self.viego.x += self.viego.dir * WALK_SPEED_PPS * game_framework.frame_time
 
-        pass
+        self.velocity -= GRAVITY * game_framework.frame_time
+        self.viego.y += self.velocity * game_framework.frame_time
 
     def draw(self):
-        f = sheet_list.viego_jump[int(self.viego.frame)]
+        f = sheet_list.viego_jump
         if self.viego.face_dir == 1:  # right
             self.viego.img.clip_draw(f[0], 1545 - f[1] - f[3], f[2], f[3], self.viego.x, self.viego.y)
-        else:  # face_dir == -1: # left
+        else:
             self.viego.img.clip_composite_draw(
                 f[0], 1545 - f[1] - f[3], f[2], f[3], 0, 'h', self.viego.x, self.viego.y, f[2], f[3])
+
 class dash:
     def __init__(self, viego):
         self.viego = viego
     def enter(self,e):
+        self.viego.is_dashing = True
         pass
     def exit(self,e):
         pass
@@ -93,7 +94,7 @@ class walk:
         self.viego = viego
 
     def enter(self,e):
-
+        self.viego.is_dashing = False
         if right_down(e) or left_up(e):
             self.viego.dir = self.viego.face_dir = 1
         elif left_down(e) or right_up(e):
@@ -109,7 +110,7 @@ class walk:
             self.viego.state_machine.cur_state = self.viego.DASH
             return
         self.viego.frame=(self.viego.frame + self.viego.WALK_FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
-        self.viego.x += self.viego.dir * RUN_SPEED_PPS * game_framework.frame_time
+        self.viego.x += self.viego.dir * WALK_SPEED_PPS * game_framework.frame_time
         pass
 
     def draw(self):
@@ -143,6 +144,7 @@ class Idle:
         self.viego = viego
 
     def enter(self,e):
+        self.viego.is_dashing = False
         self.viego.dir = 0
         self.viego.wait_start_time = get_time()
 
@@ -178,6 +180,7 @@ class Viego:
         self.SLEEP_FRAME_PER_ACTION = 7
         self.WALK_FRAME_PER_ACTION = 5
         self.DASH_FRAME_PER_ACTION = 6
+        self.JUMP_FRAME_PER_ACTION = 2
 
         self.font = load_font('ENCR10B.TTF', 16)
         self.x, self.y = 400, 90
@@ -187,20 +190,24 @@ class Viego:
 
         self.is_dashing = False
         self.is_guarding = False
+        self.is_jumping = False
 
         self.IDLE = Idle(self)
         self.WALK = walk(self)
         self.DASH = dash(self)
         self.SLEEP = Sleep(self)
         self.GUARD = guard(self)
+        self.JUMP = jump(self)
+
         self.state_machine = StateMachine(
             self.IDLE,
             {
                 self.SLEEP: {space_down: self.IDLE, right_down: self.WALK, left_down: self.WALK},
-                self.IDLE: {time_out: self.SLEEP, right_down: self.WALK, left_down: self.WALK, right_up: self.WALK, left_up: self.WALK, s_key_down: self.GUARD},
-                self.WALK: {right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE, a_key: self.DASH, s_key_down: self.GUARD},
-                self.DASH: {a_key_up: self.WALK, right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE, s_key_down: self.GUARD},
-                self.GUARD: {s_key_up: self.IDLE}
+                self.IDLE: {time_out: self.SLEEP, right_down: self.WALK, left_down: self.WALK, right_up: self.WALK, left_up: self.WALK, s_key_down: self.GUARD, up_down : self.JUMP},
+                self.WALK: {right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE, a_key: self.DASH, s_key_down: self.GUARD, up_down : self.JUMP},
+                self.DASH: {a_key_up: self.WALK, right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE, s_key_down: self.GUARD, up_down : self.JUMP},
+                self.GUARD: {s_key_up: self.IDLE},
+                self.JUMP: {}
             }
         )
 
@@ -215,7 +222,14 @@ class Viego:
 
         self.state_machine.handle_state_event(('INPUT', event))
         pass
+    def get_bb(self):
+        # 바운딩 박스 (left, bottom, right, top)
+        return (self.x - 10,
+                self.y - 22,
+                self.x + 10,
+                self.y + 10)
 
     def draw(self):
         self.state_machine.draw()
+        draw_rectangle(*self.get_bb())
         self.font.draw(self.x-20, self.y+30, f'(Time: {get_time():.2f})' , (255, 255, 0))

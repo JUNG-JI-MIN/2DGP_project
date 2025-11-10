ef1da195bd7398257139588c2dd2a38366d3cb6a
JUNG JI MIN <86883269+JUNG-JI-MIN@users.noreply.github.com>
2025-11-10 19:50:59 +0900

    몬스터 추가 및 기반 생성

--- play_mode.py
+++ play_mode.py
@@ -4,7 +4,8 @@ import logo_mode
 from viego import Viego
 import game_world
 import game_framework
-
+import stage_loader
+import monster
 running = True
 
 def handle_events():
@@ -21,10 +22,21 @@ def handle_events():
 def init():
     global running
     global world
-    global viego
-    global soop_back
+    global viego, monster
+    global soop_back, grass
+
+    soop_back = stage_loader.Background('background/soop_back.png',3)
+    game_world.add_object(soop_back,0)
+
+    monsters = [monster.Ghost() for _ in range(3)]
+    game_world.add_objects(monsters,1)
+
+    grass = stage_loader.Grass()
+    game_world.add_object(grass,0)
+
     viego = Viego()
     game_world.add_object(viego,1)
+
     pass
 def finish():
     game_world.clear()

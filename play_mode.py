6b97dcb66477cf35e3d3d917f99b9133a46a06bf
JUNG JI MIN <86883269+JUNG-JI-MIN@users.noreply.github.com>
2025-11-18 00:40:48 +0900

    스테이지 체인지

--- play_mode.py
+++ play_mode.py
@@ -8,6 +8,8 @@ import stage_loader
 import monster
 import camera
 running = True
+current_stage = 1
+current_theme = ('snow')  # 'forest', 'desert', 'castle', 'snow'
 cam = camera.Camera(2400, 600, 800, 600)
 def handle_events():
     global running
@@ -25,21 +27,18 @@ def init():
     global world
     global viego, monsters
     global soop_back, grass, cam
-    global current_stage
-
-    current_theme = 'forest'
-    current_stage = 1
+    global current_stage,current_theme
 
     # 스테이지 크기에 맞게 카메라 생성
     map_width, map_height = stage_loader.get_stage_size(current_theme, current_stage)
     cam = camera.Camera(map_width, map_height, 800, 600)
     game_world.set_camera(cam)
 
-    soop_back = stage_loader.Background('background/soop_back.png',3)
-    game_world.add_object(soop_back,0)
+    back = stage_loader.Background(f'background/{current_theme}.png')
+    game_world.add_object(back,0)
 
-    grass = stage_loader.Grass()
-    game_world.add_object(grass,0)
+    platform = stage_loader.platform(f'background/{current_theme}_platform.png',current_stage)
+    game_world.add_object(platform,0)
 
     viego = Viego()
     game_world.add_object(viego, 1)
@@ -71,3 +70,23 @@ def draw():
     update_canvas()
     pass
 
+def change_stage(theme, stage_num):
+    """스테이지 전환 함수"""
+    global current_theme, current_stage, soop_back, grass, monsters
+
+    current_theme = theme
+    current_stage = stage_num
+
+    # 1. 카메라 맵 크기 업데이트
+    map_width, map_height = stage_loader.get_stage_size(theme, stage_num)
+    cam.update_map_size(map_width, map_height)
+
+    # 2. 게임 오브젝트 재로드
+    game_world.clear_except_player()
+
+    # 배경 재로드
+    back = stage_loader.Background(f'background/{theme}_platform.png')
+    game_world.add_object(back, 0)
+
+    plat = stage_loader.platform(theme,stage_num)
+    game_world.add_object(plat, 0)

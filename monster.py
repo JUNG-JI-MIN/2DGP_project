eb51d3eb5d2f72b3d7c20aa7d8209eaa42e9abba
JUNG JI MIN <86883269+JUNG-JI-MIN@users.noreply.github.com>
2025-12-06 22:01:25 +0900

    몬스터의 지저분한 오류 수정

--- monster.py
+++ monster.py
@@ -19,28 +19,63 @@ animation_names = ['Idle']
 class fireball:
     img = None
 
-    def __init__(self, x,y):
+    def __init__(self, x, y, ghost):
         if fireball.img is None:
             fireball.img = load_image('monster/dark_ghost.png')
         self.font = load_font('ENCR10B.TTF', 16)
-
+        self.ghost = ghost
         self.frame = 0
 
         self.ATTACK_FRAME_PER_ACTION = 2
+
+        # 시작 위치
+        self.start_x = x
+        self.start_y = y
+
+        # 목표 위치 (생성 시점의 플레이어 위치)
+        self.target_x = nommor.viego.x
+        self.target_y = nommor.viego.y
+
+        # 현재 위치
         self.x = x
         self.y = y
 
+        # 매개변수 t (0 ~ 1)
+        self.t = 0.0
+        self.speed = 1  # t 증가 속도 (초당)
 
     def update(self):
-        if (self.frame <2):
+        # 프레임 애니메이션
+        if self.frame < 2:
             self.frame = (self.frame + self.ATTACK_FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time)
-        else:
+
+        # t값 증가
+        self.t += self.speed * game_framework.frame_time
+
+        # t가 1 이상이면 목표 도달
+        if self.t >= 1.0:
+            self.t = 1.0
             game_world.remove_object(self)
+            return
+
+        # 매개변수 방정식: P(t) = (1-t) * P0 + t * P1
+        self.x = (1 - self.t) * self.start_x + self.t * self.target_x
+        self.y = (1 - self.t) * self.start_y + self.t * self.target_y
     def draw(self):
         f = sheet_list.dark_ghost_attack[1][min(int(self.frame),0)]
         screen_x, screen_y = game_world.render(self, self.x, self.y)  # 카메라 좌표로 변환
 
         fireball.img.clip_draw(f[0], 949 - f[1] - f[3], f[2], f[3], screen_x, screen_y)
+
+        # 카메라 오프셋 계산
+        offset_x = screen_x - self.x
+        offset_y = screen_y - self.y
+
+        # 바운딩 박스를 카메라 좌표로 변환
+        left, bottom, right, top = self.get_bb()
+        draw_rectangle(left + offset_x, bottom + offset_y, right + offset_x, top + offset_y)
+
+
     def get_bb(self):
         return (self.x - 15,
                 self.y - 15,
@@ -52,10 +87,19 @@ class fireball:
                 self.x + 15,
                 self.y + 15)
     def handle_collision(self, group, other):
-            pass
+        if group == 'viego:monster_attack':
+            if not nommor.viego.is_guarding:
+                if nommor.viego.HP > 0:
+                    nommor.viego.HP -=  self.ghost.int / 100
+            else:
+                if nommor.viego.ste > 0:
+                    nommor.viego.ste -=  self.ghost.int / 100
+                else:
+                    if nommor.viego.HP > 0:
+                        nommor.viego.HP -= self.ghost.int / 100
+        pass
     def handle_attack_collision(self, group, other):
         pass
-
     def handle_monster_attack_collision(self,group, other):
         pass
 class Ghost:
@@ -96,7 +140,9 @@ class Ghost:
                 ITEM = item.Item(self.x - 10, self.y- 50, 'ghost')
                 game_world.add_object(ITEM)
                 game_world.add_collision_pair('viego:item', None, ITEM)
+
                 MONEY = item.Item(self.x + 10, self.y - 50, 'money')
+                MONEY.value = 500
                 game_world.add_object(MONEY)
                 game_world.add_collision_pair('viego:item', None, MONEY)
         elif self.is_attacking:
@@ -108,10 +154,9 @@ class Ghost:
                 self.face_dir = self.dir
             if int(prev) < 2 <= int(self.frame):
                 if nommor.viego:
-                    ball = fireball(nommor.viego.x, nommor.viego.y)
+                    ball = fireball(self.x, self.y, self)
                     game_world.add_object(ball)
-                    game_world.add_collision_pair('viego:ghost_attack', None, ball)
-                    game_world.add_collision_pair('viego:ghost_attack', nommor.viego, None)
+                    game_world.add_collision_pair('viego:monster_attack', None, ball)
         elif self.walk == True:
             self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time
             self.frame = (self.frame + self.RUN_FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 6
@@ -217,12 +262,12 @@ class Yeti_attack:
                 self.x + 70,
                 self.y + 30)
     def get_attack_bb(self):
-        return (self.x - 70,
-                self.y - 30,
-                self.x + 70,
-                self.y + 30)
+        return (self.x - 120,
+                self.y - 50,
+                self.x + 120,
+                self.y + 50)
     def update(self):
-        if (self.frame <5):
+        if (self.frame <4):
             self.frame = (self.frame + self.ATTACK_FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time)
         else:
             game_world.remove_object(self)
@@ -234,14 +279,21 @@ class Yeti_attack:
         offset_y = screen_y - self.y
 
         left, bottom, right, top = self.get_attack_bb()
-        draw_rectangle(left + offset_x, bottom + offset_y, right + offset_x, top + offset_y, 0, 0, 255)
+        draw_rectangle(left + offset_x, bottom + offset_y, right + offset_x, top + offset_y, 255, 0, 255)
         pass
 
     def handle_collision(self, group, other):
         if group == 'viego:monster_attack':
             game_world.remove_collision_object(self)
-            if nommor.viego.HP > 0:
-                nommor.viego.HP -=  self.yeti.int
+            if not nommor.viego.is_guarding:
+                if nommor.viego.HP > 0:
+                    nommor.viego.HP -= self.yeti.int / 100
+            else:
+                if nommor.viego.ste > 0:
+                    nommor.viego.ste -= self.yeti.int / 100
+                else:
+                    if nommor.viego.HP > 0:
+                        nommor.viego.HP -= self.yeti.int / 100
             pass
 
     def handle_attack_collision(self, group, other):
@@ -272,7 +324,7 @@ class Yeti:
         self.x = random.randint(0, 2400)
         self.mx = self.x - 500
         self.Mx = self.x + 500
-        self.y = 90
+        self.y = 50
         self.dir = 1
         self.frame = random.randint(1, 3)
         self.face_dir = 1
@@ -288,6 +340,7 @@ class Yeti:
                 game_world.add_object(ITEM)
                 game_world.add_collision_pair('viego:item', None, ITEM)
                 MONEY = item.Item(self.x + 10, self.y - 50, 'money')
+                MONEY.value = 750
                 game_world.add_object(MONEY)
                 game_world.add_collision_pair('viego:item', None, MONEY)
         elif self.is_attacking:
@@ -301,7 +354,6 @@ class Yeti:
                     attack = Yeti_attack(self.x, self.y, self)
                     game_world.add_object(attack)
                     game_world.add_collision_pair('viego:monster_attack', None, attack)
-                    game_world.add_collision_pair('viego:monster_attack', nommor.viego, None)
         elif self.walk == True:
             self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time
             self.frame = (self.frame + self.RUN_FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
@@ -358,10 +410,10 @@ class Yeti:
                 self.y + 45)
 
     def get_attack_bb(self):
-        return (self.x - 70,
-                self.y - 30,
-                self.x + 70,
-                self.y + 30)
+        return (self.x - 120,
+                self.y - 50,
+                self.x + 120,
+                self.y + 50)
 
     def handle_collision(self, group, other):
         if group == 'viego:monster':
